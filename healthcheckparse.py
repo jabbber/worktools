18d6e5bd740fb3008e0566ceeb4a1d0ebfabc84e
Jabber Zhou <mayjabber@gmail.com>
2012-12-20 05:17:11 +0800

    坑爹，才10台

--- healthcheckparse.py
+++ healthcheckparse.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os,sys
 from bs4 import BeautifulSoup
+for col in sys.argv[1:]:
+    html = file(os.path.realpath(col),'r').read().decode('utf-8')
 
+    soup = BeautifulSoup(html)
+    #help(soup.body.div)
+    for text in soup.body.div.findChildren():
+        print text.getText(),
+    print('')
