b943a5f81e76779fe1d8b5a0f6343d9531795036
Jabber Zhou <mayjabber@gmail.com>
2013-02-26 16:14:59 +0800

    重构,分离解析xlsx和构造html的代码

--- readxlsx.py
+++ readxlsx.py
@@ -3,85 +3,89 @@
 import sys
 from openpyxl.reader.excel import load_workbook
 
-
-def valformat(value):
-    if type(value) == str:
-        pass
-    elif type(value) == int:
-        value = str(value)
-    else:
-        pass
-    return value
-
-def converthtml(sheet):
+def table2html(table):
     tablestyle = 'border="1" style="border-collapse:collapse"'
-    table = 0
-    row = 0
-    row_empty = 0
-    row_max_empty = 100
-    end = False
-    output = []
-    th = None
-    while not end:
-        row += 1
-        col = []
-        col_empty = 0
-        col_max_empty = 3
-        col_end = False
-        while not col_end:
-            value = sheet.cell(row=row,column=len(col)).value
-            if value:
-                value = valformat(value)
-                col_empty = 0
-            else:
-                value = ''
-                col_empty += 1
-            col.append(value)
-            if col_empty >= col_max_empty:
-                col = col[:-col_max_empty]
-                col_end = True
-        if len(col) == 1:
-            if not th:
-                th = col[0]
-                output.append('<table %s>\n'%tablestyle)
-                output[table] += '  %s<br/>\n'%th
-            else:
-                output[table] += '</table>\n'
-                if output[table] == '<table %s>\n</table>\n'%tablestyle:
-                    output.pop()
-                else:
-                    table += 1
-                th = col[0]
-                output.append('<table %s>\n'%tablestyle)
-                output[table] += '  %s<br/>\n'%th
-        elif len(col) > 1:
-            if not th:
-                th = ""
-                output.append('<table %s>\n'%tablestyle)
-                output[table] += '  %s<br/>\n'%th
-            output[table] += '  <tr>\n'
-            output[table] += '    <td style="white-space:nowrap">%s'%('</td>\n    <td style="white-space:nowrap">'.join(col))
-            output[table] += '</td>\n  </tr>\n'
-            row_empty = 0
-        else:
-            row_empty += 1
-        if row_empty >= row_max_empty:
-            end = True
-    output[table] += '</table>\n'
-    return output
-def xlsx2html(xlsxfile,num = ['all']):
-    wb = load_workbook(filename = xlsxfile)
-    sheet_ranges = wb.get_active_sheet()
-    tables = converthtml(sheet_ranges)
+    tdstyle = 'style="white-space:nowrap"'
+    html = '<table %s>\n'%tablestyle
+    for row in table:
+        html += '  <tr>\n'
+        for val in row:
+            html += '<td %s>%s</td>\n'%(tdstyle,val)
+        html += '  </tr>\n'
+    html += '</table>'
+    return html
+
+def xlsx2html(xlsxfile,title = ['all']):
+    t = Tables()
+    t.load_xlsx(xlsxfile)
     output = ''
-    if num[0] == 'all':
-        for table in tables:
-            output += table.encode("utf-8")
-            output +=  '<br/>\n'.encode('utf-8')
+    if title[0] == 'all':
+        for title in t.titles:
+            output += ('%s\n'%title).encode('utf-8')
+            output += table2html(t.tables[title]).encode("utf-8")
+            output += '<br/>\n'.encode('utf-8')
     else:
         for n in num:
             output += tables[int(n)].encode("utf-8")
             output +=  '<br/>\n'.encode('utf-8')
     return output
+
+class Tables():
+    def __init__(self):
+        self.titles = []
+        self.tables = {}
+    def load_xlsx(self,xlsx_file):
+        wb = load_workbook(filename = xlsx_file)
+        sheet_ranges = wb.get_active_sheet()
+
+        table = 0
+        row = 0
+        row_empty = 0
+        row_max_empty = 100
+        end = False
+        th = None
+        while not end:
+            col = []
+            col_empty = 0
+            col_max_empty = 3
+            col_end = False
+            while not col_end:
+                value = sheet_ranges.cell(row=row,column=len(col)).value
+                if value:
+                    col_empty = 0
+                else:
+                    value = ''
+                    col_empty += 1
+                col.append(value)
+                if col_empty >= col_max_empty:
+                    col = col[:-col_max_empty]
+                    col_end = True
+            row += 1
+            if len(col) == 1:
+                if not th:
+                    th = col[0]
+                    worktable = []
+                else:
+                    if th == "" and worktable == []:
+                        pass
+                    else:
+                        self.titles.append(th)
+                        self.tables[th] = worktable
+                    th = col[0]
+                    worktable = []
+            elif len(col) > 1:
+                if not th:
+                    th = ""
+                    worktable = []
+                worktable.append(col)
+                row_empty = 0
+            else:
+                row_empty += 1
+            if row_empty >= row_max_empty:
+                end = True
+                self.titles.append(th)
+                self.tables[th] = worktable
+
 if __name__ == "__main__":
     print xlsx2html(sys.argv[1],sys.argv[2:])
+
