ef0e11c0b1046811a513a44cdbb473a2301cd3d8
Jabber Zhou <mayjabber@gmail.com>
2013-02-27 08:53:08 +0800

    将转换table到html的函数并入Table类方法

--- readxlsx.py
+++ readxlsx.py
@@ -3,35 +3,6 @@
 import sys
 from openpyxl.reader.excel import load_workbook
 
-def table2html(table):
-    tablestyle = 'border="1" style="border-collapse:collapse"'
-    tdstyle = 'style="white-space:nowrap"'
-    html = '<table %s>\n'%tablestyle
-    for row in table:
-        html += '  <tr>\n'
-        for val in row:
-            html += '<td %s>%s</td>\n'%(tdstyle,val)
-        html += '  </tr>\n'
-    html += '</table>'
-    return html
-
-def xlsx2html(xlsxfile,titles = ['all']):
-    t = Tables()
-    t.load_xlsx(xlsxfile)
-    output = ''
-    if titles[0] == 'all':
-        for title in t.titles:
-            output += ('%s\n'%title).encode('utf-8')
-            output += table2html(t.tables[title]).encode("utf-8")
-            output += '<br/>\n'.encode('utf-8')
-    else:
-        for title in titles:
-            title = title.decode('utf-8')
-            output += ('%s\n'%title).encode('utf-8')
-            output += table2html(t.tables[title]).encode("utf-8")
-            output += '<br/>\n'.encode('utf-8')
-    return output
-
 class Tables():
     def __init__(self):
         self.titles = []
@@ -87,6 +58,36 @@ class Tables():
                 end = True
                 self.titles.append(th)
                 self.tables[th] = worktable
+    def __table2html(self,table):
+        tablestyle = 'border="1" style="border-collapse:collapse"'
+        tdstyle = 'style="white-space:nowrap"'
+        html = '<table %s>\n'%tablestyle
+        for row in table:
+            html += '  <tr>\n'
+            for val in row:
+                html += '<td %s>%s</td>\n'%(tdstyle,val)
+            html += '  </tr>\n'
+        html += '</table>'
+        return html
+    def gethtml(self,titles = ['all']):
+        output = ''
+        if titles[0] == 'all':
+            for title in self.titles:
+                output += ('%s\n'%title).encode('utf-8')
+                output += self.__table2html(self.tables[title]).encode("utf-8")
+                output += '<br/>\n'.encode('utf-8')
+        else:
+            for title in titles:
+                title = title.decode('utf-8')
+                output += ('%s\n'%title).encode('utf-8')
+                output += self.__table2html(self.tables[title]).encode("utf-8")
+                output += '<br/>\n'.encode('utf-8')
+        return output
+
+def xlsx2html(xlsxfile,titles = ['all']):
+    t = Tables()
+    t.load_xlsx(xlsxfile)
+    return t.gethtml(titles)
 
 if __name__ == "__main__":
     print xlsx2html(sys.argv[1],sys.argv[2:])
