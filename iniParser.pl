fca8961bad088ca7bdd643e221342ad7f08164ad
Jabber Zhou <mayjabber@gmail.com>
2014-07-04 09:31:15 +0800

    改进性能，不再把整个文件读到内存中

--- iniParser.pl
+++ iniParser.pl
@@ -9,13 +9,9 @@ package IniParser;
         my $type = shift;
         my $this = {};
         $this->{'iniFile'} = shift;
-        $this->{'iniString'} = "";
         $this->{'err'} = "";
         open CONFIG,"$this->{'iniFile'}" or $this->{'err'} = "open $this->{'iniFile'}: $!";
         if (! $this->{'err'}){
-            while( my $string = <CONFIG>){
-                $this->{'iniString'} .= $string;
-            }
             close CONFIG;
         }
         bless $this, $type;
@@ -28,27 +24,30 @@ package IniParser;
         my $session = shift;
         my $key = shift;
         my $flag = 0;
-        my @lines = split(/\n/, $this->{'iniString'});
-        foreach my $string (@lines){
-            chomp $string;
-            if($flag == 0){
-                if ( $string =~ /^\[$session\]/ ){
-                $flag = 1;
-                }
-            }else{
-                if( $string =~ /^\[/){
-                last;}
-                if ( $string =~ /^#/){
-                    next;
-                }
-                if ($string =~ /^(\w+)=(.*)/){
-                    my $k = $1;
-                    my $v = $2;
-                    if($k eq $key){
-                        return $v;
+        open CONFIG,"$this->{'iniFile'}" or $this->{'err'} = "open $this->{'iniFile'}: $!";
+        if (! $this->{'err'}){
+            while (my $string = <CONFIG>){
+                chomp $string;
+                if($flag == 0){
+                    if ( $string =~ /^\[$session\]/ ){
+                    $flag = 1;
+                    }
+                }else{
+                    if( $string =~ /^\[/){
+                    last;}
+                    if ( $string =~ /^#/){
+                        next;
+                    }
+                    if ($string =~ /^(\w+)=(.*)/){
+                        my $k = $1;
+                        my $v = $2;
+                        if($k eq $key){
+                            return $v;
+                        }
                     }
                 }
             }
+            close CONFIG;
         }
         return "";
     }
@@ -61,7 +60,7 @@ if ($conf->{'err'}){
     exit 1;
 }
 if ($conf->val($session, $key)){
-    print "I find $key ............... value ". $conf->val($session, $key) .".\n";
+    print "I find [$session] $key 's value ". $conf->val($session, $key) .".\n";
 }else{
-    print "I can not find $key ............... value.\n";
+    print "I can not find [$session] $key 's value.\n";
 }
