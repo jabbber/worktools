8afb097e6648c5eaf41db0153da88ec33d82c6ed
Jabber Zhou <mayjabber@gmail.com>
2015-01-07 17:25:48 +0800

    modify regexp

--- jabber.pac.js
+++ jabber.pac.js
@@ -10,6 +10,7 @@ var base_Proxy = "PROXY 127.0.0.1:8118;";
 var host_list = heredoc(function(){/*
 twitter.com
 */});
+host_list = host_list.split("\n");
 
 var url_list = heredoc(function(){/*
 http://ipaddress.com/
@@ -41,11 +42,12 @@ function listmatch(value,list) {
 function regmatch(value) {
     for (var n in regexp_list)
     {
-        if (list[n] != '')
+        if (regexp_list[n] != '')
         {
             try {
-                return new RegExp(list[n]).test(value);
-            } catch(e){}
+                re = new RegExp(regexp_list[n]);
+                if (re.test(value)) {return true;}
+            } catch(e){continue;}
         }
     }
     return false;
@@ -53,10 +55,10 @@ function regmatch(value) {
 
 function FindProxyForURL(url, host)
 {
-    if (listmatch(host,host_list)) return SSL_Proxy;
-    if (listmatch(url,url_list)) return SSL_Proxy;
-    if (regmatch(url)) return SSL_Proxy;
+    if (listmatch(host,host_list)) {return SSL_Proxy;}
+    if (listmatch(url,url_list)) {return SSL_Proxy;}
+    if (regmatch(url)) {return SSL_Proxy;}
 
-return index_Proxy;
+    return index_Proxy;
 }
 
