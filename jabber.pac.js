58349d1899736da658cdbeb81ca0f213491057b1
Jabber Zhou <mayjabber@gmail.com>
2015-01-08 09:44:53 +0800

    use object hasOwnProperty instead of for array

--- jabber.pac.js
+++ jabber.pac.js
@@ -1,52 +1,81 @@
 /*
 Proxy auto-config script
 author: Jabber Zhou
-contact: mayjabber@gmail.com
+contact: mayjabber#gmail.com
 */
 var index_Proxy = "DIRECT;";
 var SSL_Proxy = "SOCKS5 127.0.0.1:1080; SOCKS5 127.0.0.1:10000;";
 var base_Proxy = "PROXY 127.0.0.1:8118;";
 
-var host_list = heredoc(function(){/*
+var domain_list = heredoc(function(){/*
 twitter.com
+youtube.com
+ytimg.com
 */});
-host_list = host_list.split("\n");
 
 var url_list = heredoc(function(){/*
 http://ipaddress.com/
 https://www.google.com/search?
 http://www.google.com/url?
 */});
-url_list = url_list.split("\n");
 
 var regexp_list = heredoc(function(){/*
 .+facebook\.com.*
+https?://www.google.com.*
 */});
+
+
+domain_list = domain_list.split("\n");
+var domain_object = {};
+for (var n in domain_list) {
+    if (domain_list[n] !== '') {
+        domain_object[domain_list[n]] = true;
+    }
+};
+
+url_list = url_list.split("\n");
 regexp_list = regexp_list.split("\n");
 
 function heredoc(fn) {
     return fn.toString().split('\n').slice(1,-1).join('\n') + '\n'
 }
 
-function listmatch(value,list) {
-    for (var n in list)
-    {
-        if (list[n] !== '')
-        {
-            if (value.indexOf(list[n]) !== -1) return true;
+var hasOwnProperty = Object.hasOwnProperty;
+function domainMatch(host) {
+    var suffix;
+    var pos = host.lastIndexOf('.');
+    pos = host.lastIndexOf('.', pos - 1);
+    while(1) {
+        if (pos <= 0) {
+            if (hasOwnProperty.call(domain_object, host)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        suffix = host.substring(pos + 1);
+        if (hasOwnProperty.call(domain_object, suffix)) {
+            return true;
+        }
+        pos = host.lastIndexOf('.', pos - 1);
+    }
+}
+
+function urlMatch(url) {
+    for (var n in url_list){
+        if (url_list[n] !== ''){
+            if (url.indexOf(url_list[n]) !== -1) return true;
         }
     }
     return false;
 }
 
-function regmatch(value) {
-    for (var n in regexp_list)
-    {
-        if (regexp_list[n] !== '')
-        {
+function regMatch(url) {
+    for (var n in regexp_list){
+        if (regexp_list[n] !== ''){
             try {
                 re = new RegExp(regexp_list[n]);
-                if (re.test(value)) {return true;}
+                if (re.test(url)) {return true;}
             } catch(e){continue;}
         }
     }
@@ -55,9 +84,9 @@ function regmatch(value) {
 
 function FindProxyForURL(url, host)
 {
-    if (listmatch(host,host_list)) {return SSL_Proxy;}
-    if (listmatch(url,url_list)) {return SSL_Proxy;}
-    if (regmatch(url)) {return SSL_Proxy;}
+    if (domainMatch(host)) {return SSL_Proxy;}
+    if (urlMatch(url)) {return SSL_Proxy;}
+    if (regMatch(url)) {return SSL_Proxy;}
 
     return index_Proxy;
 }
