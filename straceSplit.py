59d637a531337d7d7bf874151eb94b7070c1a2b7
Jabber Zhou <mayjabber@gmail.com>
2015-02-28 10:48:51 +0800

    thread for progress

--- straceSplit.py
+++ straceSplit.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import os
 import sys
+import time
 import threading
 
 if len(sys.argv) != 3:
@@ -19,12 +20,24 @@ if not os.path.isdir(outputdir):
     print("'%s' is not a directory!"%outputdir)
     sys.exit(2)
 
+class Progress (threading.Thread):
+    def __init__(self, fd, fsize):
+        threading.Thread.__init__(self)
+        self.fd = fd 
+        self.fsize = fsize//100
+    def run(self):
+        while not self.fd.closed:
+            print('{}%'.format(self.fd.tell()//self.fsize),end='\r',file=sys.stderr)
+            time.sleep(1)
+
 tmp = {}
 size = os.path.getsize(stracefile)
 
 print('parse...',file=sys.stderr)
 with open(stracefile) as stracef:
     line = True
+    myProgress = Progress(stracef,size)
+    myProgress.start()
     while line:
         line = stracef.readline()
         try:
@@ -36,7 +49,5 @@ with open(stracefile) as stracef:
         else:
             tmp[tid] = open(os.path.join(outputdir,os.path.basename(stracefile)+'.'+str(tid)),'w')
         tmp[tid].write(line)
-        if stracef.tell()%100000 == 0:
-            print('%d'%stracef.tell(),end='\r',file=sys.stderr)
 print('\nfinished!',file=sys.stderr)
 
