80f901f2fc4a75bb944d2313297383f1c9acb9bc
Jabber Zhou <mayjabber@gmail.com>
2015-03-19 15:31:38 +0800

    ignore all number diff

--- duplicate.py
+++ duplicate.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 from openpyxl import load_workbook
+import re
 wb= load_workbook('周期性的日志分析.xlsx')
 sheets = {}
 for sheet in wb.get_sheet_names():
     ws = wb.get_sheet_by_name(sheet)
     ws.get_cell_collection()
-    table = tuple((cell.value for cell in ws.get_cell_collection()))
+    table = ''.join((cell.value for cell in ws.get_cell_collection() if type(cell.value) in (str,unicode)))
+    table = re.sub('\d+','',table)
     if not table in sheets:
         sheets[table] = [sheet]
     else:
@@ -15,9 +17,10 @@ for sheet in wb.get_sheet_names():
 duplicate = []
 for sheet in sheets.values():
     if len(sheet) > 1:
+        duplicate.append('')
         duplicate.extend(sheet)
 
-duplicate.sort()
+#duplicate.sort()
 for sheet in duplicate:
     print(sheet)
 
