0b5acd3d16c5c01c7e9c9241844f57728e0d65f6
Jabber Zhou <mayjabber@gmail.com>
2016-08-29 15:33:00 +0800

    zypper la can not see all, pa is better

--- zypper_join.py
+++ zypper_join.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #author: zwj
-#this script can join zypper pch and lu
+#this script can join zypper pch and pa
 import sys
 import re
 with open('pch.txt') as pchfile:
@@ -9,7 +9,7 @@ patchnames = {}
 for line in pch.split("\n")[4:]:
     if re.match("^\s*$",line):
         continue
-    patchname = re.findall("^[\w-]+\s+\|\s+\w+-((?:\w+-)*[a-zA-Z]\w+)(?:-\d\w*)*-\d+\s+\|", line)
+    patchname = re.findall("^[\w-]+\s+\|\s+\w+-((?:[\w+]+-)*[a-zA-Z][\w+]+)(?:-\d\w*)*(?:-\d+)?\s+\|", line)
     if len(patchname) == 1:
         if patchname[0]:
             #print patchname[0]
@@ -17,17 +17,17 @@ for line in pch.split("\n")[4:]:
             continue
     sys.stderr.write("This line in 'pch' can not match:\n'>>'%s\n"%line)
 
-with open('lu.txt') as lufile:
-    lu = lufile.read()
-for line in lu.split("\n")[4:]:
+with open('pa.txt') as lufile:
+    pa = pafile.read()
+for line in pa.split("\n")[4:]:
     if re.match("^\s*$",line):
         continue
-    updatename = re.findall("\S+\s+\|\s+[\w-]+\s+\|\s+((?:\w+-)*\w+)\s+\|", line)
+    updatename = re.findall(".+\s+\|\s+.+\s+\|\s+((?:[\w+]+-)*[\w+]+)\s+\|", line)
     if len(updatename) == 1:
         if updatename[0]:
             #print updatename[0]
             if updatename[0] in patchnames:
-                print line + patchnames[updatename[0]]
+                print line + patchnames.pop(updatename[0])
             continue
-    sys.stderr.write("This line in 'pch' can not match:\n'>>'%s\n"%line)
+    sys.stderr.write("This line in 'pa' can not match:\n'>>'%s\n"%line)
 
