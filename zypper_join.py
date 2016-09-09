088a293018c25e1ab3da14b6eb8f1cb0e5b4deb3
Jabber Zhou <mayjabber@gmail.com>
2016-09-09 10:59:33 +0800

    find the newest version of patch

--- zypper_join.py
+++ zypper_join.py
@@ -9,15 +9,20 @@ patchnames = {}
 for line in pch.split("\n")[4:]:
     if re.match("^\s*$",line):
         continue
-    patchname = re.findall("^[\w-]+\s+\|\s+\w+-((?:[\w+]+-)*[a-zA-Z][\w+]+)(?:-\d\w*)*(?:-\d+)?\s+\|", line)
+    patchname = re.findall("^[\w-]+\s+\|\s+\w+-((?:[\w+]+-)*[a-zA-Z][\w+]+)(?:-\d\w*)*-(\d+)\s+\|", line)
     if len(patchname) == 1:
         if patchname[0]:
-            #print patchname[0]
-    	    patchnames[patchname[0]] = line
+            patch = patchname[0][0]
+            version = patchname[0][1]
+            if patch in patchnames:
+                if version > patchnames[patch][0]:
+                    patchnames[patch] = (version,line)
+            else:
+                patchnames[patch] = (version,line)
             continue
     sys.stderr.write("This line in 'pch' can not match:\n'>>'%s\n"%line)
 
-with open('pa.txt') as lufile:
+with open('pa.txt') as pafile:
     pa = pafile.read()
 for line in pa.split("\n")[4:]:
     if re.match("^\s*$",line):
@@ -27,7 +32,7 @@ for line in pa.split("\n")[4:]:
         if updatename[0]:
             #print updatename[0]
             if updatename[0] in patchnames:
-                print line + patchnames.pop(updatename[0])
+                print line + patchnames.pop(updatename[0])[1]
             continue
     sys.stderr.write("This line in 'pa' can not match:\n'>>'%s\n"%line)
 
