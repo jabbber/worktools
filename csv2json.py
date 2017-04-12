6c6df01257023c18893359746fa16b3f2216d6fa
Jabber Zhou <mayjabber@gmail.com>
2017-04-12 15:12:31 +0800

    精简和优雅

--- csv2json.py
+++ csv2json.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python2
+## -*- coding: utf-8 -*-
 import csv
 import json
 import sys
+import commands
 
-with open(sys.argv[1]) as f:
-    reader = csv.DictReader(f)
-    rows = list(reader)
+def jsondump(item):
+    return json.dumps(item, sort_keys=True,indent=4).decode('unicode_escape').encode('utf-8')
 
-for row in rows:
-    a = json.dumps(row, sort_keys=True,indent=4)
-    print a.decode('unicode_escape').encode('utf-8') + ','
+if __name__ == "__main__":
+    with open(sys.argv[1]) as f:
+        reader = csv.DictReader(f)
+        rows = list(reader)
+
+    for doc in rows:
+        print jsondump(doc) + ','
