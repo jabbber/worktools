b459c0b08b780e4e1135a6bd127e876e07a24aa3
jabber <mayjabber@gmail.com>
2017-08-17 17:39:10 +0800

    hbase 状态获取

---
+++ hbased_status.py
@@ -0,0 +1,40 @@
+import requests
+import re
+import sys
+import json
+
+dump_url = "http://10.214.160.196:60010/dump"
+
+hbase_dump = requests.get(dump_url)
+
+if not hbase_dump.ok:
+    exit(1)
+
+def sectionCut(text,name):
+    section = re.findall('''
+        %s:\\n
+        =+\\n
+        (.+?)
+        \\n{2,}
+        .+:\\n
+        =+
+        '''%name,text,flags=re.VERBOSE+re.DOTALL)
+    if len(section) == 1:
+        return section[0]
+    else:
+        sys.stderr.write('"Servers" sections cannot find!\n')
+        exit(2)
+
+region_status = sectionCut(hbase_dump.text,'Servers').split('\n')
+
+regions = {}
+for row in region_status:
+    name = row[:row.find(':')]
+    data = row[row.find(': ')+1:]
+    data = re.sub('([a-zA-Z]\w+)(?![=\w])','"\\1"',data)
+    data = data.replace('"NaN"','None')
+    regions[name] = eval("dict(%s)"%data)
+
+print json.dumps(regions,sort_keys=True,indent=4)
+
+
